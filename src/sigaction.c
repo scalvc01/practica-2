c28a9e410d3e9b7eed9debdc65c4bce9e31caf9d
am6 <am.guerrero@unileon.es>
2017-02-27 12:09:08 +0100

    Corrección ejemplos

--- src/sigaction.c
+++ src/sigaction.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <signal.h>
 
+struct sigaction sa;
+
 void handler (int sig) {
   printf ("SIGINT received\n");
 }
 
 int main(void) {
-  struct sigaction sa;
-
   sa.sa_handler = handler;
 
   sigaction (SIGINT, &sa, NULL);
